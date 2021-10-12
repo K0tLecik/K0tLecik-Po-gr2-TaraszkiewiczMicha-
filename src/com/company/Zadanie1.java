97b648abb44e644956727e17473b6c0852c1da7d
K0tLecik <misiu12525@wp.pl>
2021-10-12 23:11:12 +0200

    Initial commit

---
+++ src/com/company/Zadanie1.java
@@ -0,0 +1,69 @@
+package proj1;
+
+public class Zadanie1 {
+
+    public static void main(String[] args) {
+        //zad1
+        System.out.println(31+29+31);
+        //zad2
+        int suma=0;
+        for (int i=0;i<=10;i++)
+        {
+            suma+=i;
+        }
+        System.out.println(suma);
+        //zad3
+        int ilocz=1;
+        for (int i=1;i<=10;i++)
+        {
+            ilocz *= i;
+        }
+        System.out.println(ilocz);
+        //zad4
+        float saldoKonta=1000;
+        for(int i=1;i<=3;i++)
+        {
+            if(i>1 && i<=3)
+            {
+                saldoKonta+=saldoKonta*0.06;
+            }
+            System.out.println( i + " rok = "+saldoKonta);
+        }
+        //zad5
+        System.out.println("______\n|JAVA|\n______");
+        //zad6
+        System.out.println("   /////    \n  +\"\"\"\"\"+  \n (| o o |)\n   | ^ |   \n  | `-' |  \n  +-----+  \n");
+        //zad7
+        System.out.println("*   *    *           *              **\n*   *                *               *\n** **   **     ***   * **    ***     *\n* * *    *    *   *  *   *      *    *\n*   *    *    *      *   *   ****    *\n*   *    *    *   *  *   *  *   *    *\n*   *   ***    ***   *   *   ****   ***\n");
+        //zad8
+        System.out.println("   +    \n  + +   \n +   +  \n+-----+\n| .-. |\n| | | |\n+-+-+-+\n");
+        //zad9
+        System.out.println(" /\\_/\\ \n( ' ' )        -----\n(  -  )      / Hello \\\n | | |     <   Junior |\n(__|__)      \\ Coder!/\n               -----");
+        //zad10
+        System.out.println("1.Star Wars\n2.Krecik\n3.Indiana Jones");
+        //zad11
+        System.out.println("Dłoń \n" +
+                "Dwadzieścia siedem kości,\n" +
+                "trzydzieści pięć mięśni,\n" +
+                "około dwóch tysięcy komórek nerwowych\n" +
+                "w każdej opuszce naszych pięciu palców.\n" +
+                "To zupełnie wystarczy,\n" +
+                "żeby napisać „Mein Kampf”\n" +
+                "albo „Chatkę Puchatka”.");
+        //zad12
+        System.out.println("* * * * * * =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println("* * * * * * =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println("* * * * * * =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println("* * * * * * =========================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+    }
+}
