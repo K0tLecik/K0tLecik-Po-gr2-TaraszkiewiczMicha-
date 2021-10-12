57e200260addf8d81fbd329a4c2947acefc08441
K0tLecik <misiu12525@wp.pl>
2021-10-12 23:21:08 +0200

    as

--- src/com/company/Zadanie1.java
+++ src/com/company/Zadanie1.java
@@ -42,14 +42,25 @@ public class Zadanie1 {
         //zad10
         System.out.println("1.Star Wars\n2.Krecik\n3.Indiana Jones");
         //zad11
-        System.out.println("Dłoń \n" +
-                "Dwadzieścia siedem kości,\n" +
-                "trzydzieści pięć mięśni,\n" +
-                "około dwóch tysięcy komórek nerwowych\n" +
-                "w każdej opuszce naszych pięciu palców.\n" +
-                "To zupełnie wystarczy,\n" +
-                "żeby napisać „Mein Kampf”\n" +
-                "albo „Chatkę Puchatka”.");
+        System.out.println("Okulary\n" +
+                "Biega, krzyczy pan Hilary:\n" +
+                "Gdzie są moje okulary?\n" +
+                "Szuka w spodniach i w surducie,\n" +
+                "W prawym bucie, w lewym bucie.\n" +
+                "Wszystko w szafach poprzewracał,\n" +
+                "Maca szlafrok, palto maca.\n" +
+                "Skandal! - krzyczy - nie do wiary!\n" +
+                "Ktoś mi ukradł okulary!\n" +
+                "Pod kanapą, na kanapie,\n" +
+                "Wszędzie szuka, parska, sapie!\n" +
+                "Szuka w piecu i w kominie,\n" +
+                "W mysiej dziurze i w pianinie.\n" +
+                "Już podłogę chce odrywać,\n" +
+                "Już policję zaczął wzywać.\n" +
+                "Nagle zerknął do lusterka...\n" +
+                "Nie chce wierzyć... Znowu zerka.\n" +
+                "Znalazł! Są! Okazało się,\n" +
+                "Że je ma na własnym nosie.\n");
         //zad12
         System.out.println("* * * * * * =========================================================");
         System.out.println(" * * * * *  =========================================================");
